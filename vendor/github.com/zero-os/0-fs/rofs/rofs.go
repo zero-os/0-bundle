ce133267874b374fb8e338e0c5f30dbb01e55e88
Muhamad Azmy <muhamada@greenitglobe.com>
2018-03-21 16:37:55 +0200

    Update vendor

--- vendor/github.com/zero-os/0-fs/rofs/rofs.go
+++ vendor/github.com/zero-os/0-fs/rofs/rofs.go
@@ -1,14 +1,12 @@
 package rofs
 
 import (
-	"fmt"
-	"github.com/zero-os/0-fs/meta"
-	"github.com/zero-os/0-fs/storage"
 	"github.com/hanwen/go-fuse/fuse"
 	"github.com/hanwen/go-fuse/fuse/nodefs"
 	"github.com/hanwen/go-fuse/fuse/pathfs"
 	"github.com/op/go-logging"
-	"math"
+	"github.com/zero-os/0-fs/meta"
+	"github.com/zero-os/0-fs/storage"
 )
 
 const (
@@ -35,48 +33,6 @@ func New(storage storage.Storage, store meta.MetaStore, cache string) pathfs.Fil
 	}
 
 	return pathfs.NewReadonlyFileSystem(fs)
-}
-
-func (fs *filesystem) GetAttr(name string, context *fuse.Context) (*fuse.Attr, fuse.Status) {
-	log.Debugf("GetAttr %s", name)
-	m, ok := fs.store.Get(name)
-	if !ok {
-		return nil, fuse.ENOENT
-	}
-
-	info := m.Info()
-	if info.Type == meta.UnknownType {
-		return nil, fuse.EIO
-	}
-
-	nodeType := uint32(info.Type)
-
-	access := info.Access
-
-	blocks := uint64(math.Ceil(float64(info.Size / blkSize)))
-
-	var major, minor uint32
-	if info.SpecialData != "" {
-		fmt.Sscanf(info.SpecialData, "%d,%d", &major, &minor)
-	}
-
-	size := info.Size
-	if info.Type == meta.LinkType {
-		size = uint64(len(info.LinkTarget))
-	}
-
-	return &fuse.Attr{
-		Size:   size,
-		Mtime:  uint64(info.ModificationTime),
-		Mode:   nodeType | access.Mode,
-		Blocks: blocks,
-		Owner: fuse.Owner{
-			Uid: access.UID,
-			Gid: access.GID,
-		},
-		Rdev:    major<<8 | minor,
-		Blksize: blkSize, //4K blocks
-	}, fuse.OK
 }
 
 func (fs *filesystem) Open(name string, flags uint32, context *fuse.Context) (nodefs.File, fuse.Status) {
